a37280a032eb5ad4cc5bcf0bd5c213cb1e156300
Christian Roessner <c@roessner.co>
2023-12-08 08:08:30 +0100

    Fix 'Edit this page' link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/croessner/nauthilus-website',
+            'https://github.com/croessner/nauthilus-website/tree/main',
         },
         /*
         blog: {
